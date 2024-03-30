3e4d362b623cd972dae81cb264abe78201c4b2d8
yingzhang123 <1257895384@qq.com>
2024-03-30 16:16:46 +0800

    mymoduo

---
+++ noncopyable.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+/**
+ * noncopyable被继承以后，派生类对象可以正常的构造和析构，但是派生类对象
+ * 无法进行拷贝构造和赋值操作
+ */ 
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+protected:
+    noncopyable() = default;         // 使用默认构造
+    ~noncopyable() = default;
+};
